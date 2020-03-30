ecfd29f5d5e36a753c02d830c972a1ea31a4e615
huangtao <huangtao@dianmi365.com>
2020-03-31 00:24:23 +0800

    第102, 二叉树的层序遍历

---
+++ 二叉树操作/102_二叉树的层序遍历/binary_tree_level_order_traversal.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+# 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+# 示例：
+# 二叉树：[3,9,20,null,null,15,7],
+#
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# 返回其层次遍历结果：
+#
+# [
+#   [3],
+#   [9,20],
+#   [15,7]
+# ]
+
+
+# 解题思路
+# 首先确认树非空，然后调用递归函数 helper(node, level)，参数是当前节点和节点的层次。程序过程如下：
+# 输出列表称为 levels，当前最高层数就是列表的长度 len(levels)。比较访问节点所在的层次 level 和当前最高层次 len(levels) 的大小，如果前者更大就向 levels 添加一个空列表。
+# 将当前节点插入到对应层的列表 levels[level] 中。
+# 递归非空的孩子节点：helper(node.left / node.right, level + 1)
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.ret_list(root, 0, res)
+        return res
+
+    def ret_list(self, root, level, res):
+        if not root: return
+        if len(res) < level + 1:
+            res.append([])
+        self.ret_list(root.left, level + 1, res)
+        self.ret_list(root.right, level + 1, res)
